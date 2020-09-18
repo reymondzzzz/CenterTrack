0139a2d83d045224b99fe390b30a9f923e58ee45
reymondzzzz <starkov.kirill123@gmail.com>
2020-09-18 14:07:50 +0300

    wider

--- src/lib/dataset/datasets/coco.py
+++ src/lib/dataset/datasets/coco.py
@@ -16,7 +16,7 @@ class COCO(GenericDataset):
   num_categories = 1
   # num_categories = 80
   class_name = [
-    'person',
+    'face',
     #   'bicycle', 'car', 'motorcycle', 'airplane',
     # 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant',
     # 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse',
@@ -32,7 +32,7 @@ class COCO(GenericDataset):
     # 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
   ]
   _valid_ids = [
-      1,
+      0,
       # 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 13,
       # 14, 15, 16, 17, 18, 19, 20, 21, 22, 23,
       # 24, 25, 27, 28, 31, 32, 33, 34, 35, 36,
@@ -46,7 +46,7 @@ class COCO(GenericDataset):
   num_joints = 17
   flip_idx = [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10], 
               [11, 12], [13, 14], [15, 16]]
-  edges = [[0, 1], [0, 2], [1, 3], [2, 4], 
+  edges = [[0, 1], [0, 2], [1, 3], [2, 4],
            [4, 6], [3, 5], [5, 6], 
            [5, 7], [7, 9], [6, 8], [8, 10], 
            [6, 12], [5, 11], [11, 12], 
